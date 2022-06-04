18105d072260df902dd88cd16654c84cfac443a3
quang180898 <dangquang.180898@gmail.com>
2022-06-04 16:10:30 +0700

    test

--- config/settings.py
+++ config/settings.py
@@ -88,6 +88,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     'postgres_db': POSTGRES_DATABASE
 # }
 
+DATABASES = {
+    "default": {
+            'ENGINE': 'django.contrib.gis.db.backends.postgis',
+            'NAME': 'd741qslifn6jst',
+            'USER': 'owhkqpvtsnatvj',
+            'PASSWORD': '5b3e2ccc87e158295968c49a8dfb6cdeef24da992e95638aeba8fdf00e64e25e',
+            'HOST': 'ec2-54-147-33-38.compute-1.amazonaws.com',
+            'PORT': 5432
+      }
+}
+
 # Multi db:
 options.DEFAULT_NAMES = options.DEFAULT_NAMES + ('using',)
 
