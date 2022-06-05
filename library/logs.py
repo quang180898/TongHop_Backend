289f12b43ef4e963cc356c6e32cbbca7b5b04060
quang180898 <dangquang.180898@gmail.com>
2022-06-05 13:58:49 +0700

    test

--- library/logs.py
+++ library/logs.py
@@ -1,30 +1,30 @@
-# import logging
-# from config.root_local import LOGGER_PRINT
+import logging
+from config.root_local import LOGGER_PRINT
 
 
-# def logger(string, log_name='service', type='info'):
-#     if LOGGER_PRINT:
-#         logger = logging.getLogger(log_name)
+def logger(string, log_name='service', type='info'):
+    if LOGGER_PRINT:
+        logger = logging.getLogger(log_name)
 
-#         file_log_handler = logging.FileHandler('media/log/{}.log'.format(log_name))
+        file_log_handler = logging.FileHandler('media/log/{}.log'.format(log_name))
 
-#         logger.addHandler(file_log_handler)
+        logger.addHandler(file_log_handler)
 
-#         formatter = logging.Formatter('%(asctime)-15s - %(levelname)s - %(message)s')
-#         file_log_handler.setFormatter(formatter)
+        formatter = logging.Formatter('%(asctime)-15s - %(levelname)s - %(message)s')
+        file_log_handler.setFormatter(formatter)
 
-#         # stderr_log_handler = logging.StreamHandler()
-#         # logger.addHandler(stderr_log_handler)
+        # stderr_log_handler = logging.StreamHandler()
+        # logger.addHandler(stderr_log_handler)
 
-#         # nice output format
+        # nice output format
 
-#         # stderr_log_handler.setFormatter(formatter)
+        # stderr_log_handler.setFormatter(formatter)
 
-#         if type == 'info':
-#             logger.info(string)
-#         elif type == 'error':
-#             logger.error(string)
+        if type == 'info':
+            logger.info(string)
+        elif type == 'error':
+            logger.error(string)
 
-#         logger.handlers.pop()
-#     else:
-#         print(string)
+        logger.handlers.pop()
+    else:
+        print(string)
