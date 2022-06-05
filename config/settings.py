10b3686191b04b2483c4dafc945e6b5ff1e005e5
quang180898 <dangquang.180898@gmail.com>
2022-06-05 12:05:58 +0700

    test

--- config/settings.py
+++ config/settings.py
@@ -175,8 +175,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
